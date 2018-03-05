dce38f0279aa963aa65f4e88fa7aec82e9b65c57
linrui <linrui@gridsum.com>
2018-03-05 21:56:58 +0800

    add profile

---
+++ index.js
@@ -0,0 +1,18 @@
+const MD = require('markdown-it')
+const md = new MD()
+const fs = require('fs')
+const profile = fs.readFileSync('./profile.md', 'utf-8')
+const html = md.render(profile)
+const express = require('express')
+const app = express()
+
+console.log(profile)
+console.log(html)
+
+app.use('*', (req, res) => {
+  res.send(html)
+})
+
+app.listen('8080', () => {
+  console.log('start in localhost:8080')
+})
